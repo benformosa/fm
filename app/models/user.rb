35ae033fb39e7477cf17ac3b5fa3012dbcb631d6
Ben Formosa <ben.formosa@gmail.com>
2014-03-09 03:03:16 +1100

    can now login with login instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable
 
   has_many :trips
   has_many :cars, through: :trips
