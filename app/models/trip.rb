6d74f12316434282441b41560d55ee3feb776441
Ben Formosa <ben.formosa@gmail.com>
2014-05-04 20:01:47 +1000

    fix trip validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ActiveRecord::Base
   belongs_to :last_trip, :class_name => "Trip",
     :foreign_key => "last_trip"
+
   validate :greater_than_last_trip
     
   belongs_to :car
@@ -14,7 +15,7 @@ class Trip < ActiveRecord::Base
 
   #validate that the odometer reading has gone up, except for the initial trip
   def greater_than_last_trip
-    unless Trip.where(car_id: car).empty?
+    unless Trip.where(car_id: car).empty? or odo.nil?
       if last_trip.odo >= odo
         errors.add(:base, "Odometer reading must be greater than the reading from the last trip")
       end
