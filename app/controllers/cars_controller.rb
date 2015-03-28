972f91d221e40409a73aa9c0f2a031c256d82b7e
Ben Formosa <ben.formosa@gmail.com>
2015-03-28 14:39:46 +1100

    show car working

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,15 +1,16 @@
 class CarsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
 
   def index
     @cars = Car.all
   end
-  
+
   def new
     @car = Car.new
     authorize @car, :modify?
   end
-  
+
   def create
     @car = Car.new(car_params)
     authorize @car, :modify?
@@ -19,13 +20,11 @@ class CarsController < ApplicationController
       render :action => :new
     end
   end
-  
+
   def show
-    @car = Car.find(car_params[:id])
   end
 
   def edit
-    @car = Car.find(car_params[:id])
     authorize @car, :modify?
   end
 
@@ -56,10 +55,15 @@ class CarsController < ApplicationController
       @car = Car.all
     end
   end
-  
+
   private
-    def car_params
-      params.require(:car).permit(:id, :name, :make, :model, :rego, :state, :start_odo)
-      #params.permit(:car, :id, :name, :make, :model, :rego, :state, :start_odo)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:name, :make, :model, :rego, :state, :start_odo, :user_id)
+    #params.permit(:car, :id, :name, :make, :model, :rego, :state, :start_odo)
+  end
 end
