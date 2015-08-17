fbf399dec8f27c7053b86064535b0f6b25111378
Ben Formosa <ben.formosa@gmail.com>
2015-08-17 20:25:00 +1000

    config file for initscript

---
+++ install/etc-fml.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Name of app directory
+APP=fml
+
+# Port to listen on
+PORT=443
+
+# Enable SSL. true or false
+SSL=true
+
+# Path to SSL certificate
+SSL_CERT=/etc/ssl/certs/self-signed.pem
+
+# Path to private key for SSL certificate
+SSL_KEY=/etc/ssl/private/self-signed.key
+
+# User to run as
+USER=fml
