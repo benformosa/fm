f103459abfc440f45eee9434097c410ee3056c0b
Ben Formosa <ben.formosa@gmail.com>
2015-08-17 15:27:52 +1000

    updated installer

--- install.sh
+++ install.sh
@@ -3,22 +3,25 @@
 # Install FML application on Phusion Passenger
 # Tested on Debian 8.1
 
-pushd ~
+# Create user to run application as
+sudo useradd --user-group fml --home-dir /srv/rails/fml --shell /bin/bash --groups sudo
+echo 'Set password for user fml'
+sudo passwd fml
 
-echo "### Install Phusion Passenger"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
-sudo apt-get -y install apt-transport-https ca-certificates
-echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger jessie main' | sudo tee --append /etc/apt/sources.list.d/passenger.list > /dev/null
+# Create application directory
+sudo mkdir -p /srv/rails/fml
+sudo chown fml:fml /srv/rails/fml
+
+# Install pre-requisite software
 sudo apt-get update
-sudo apt-get install -y passenger
+sudo apt-get install -y ruby ruby-dev zlib1g-dev libsqlite3-dev openssl git
 
-echo "### Create self-signed certificate"
-sudo openssl req -new -x509 -days 365 -nodes -out /etc/ssl/certs/self-signed.pem -keyout /etc/ssl/private/self-signed.key
+# Generate a self-signed certificate
+sudo openssl req -new -x509 -days 365 -nodes -out /etc/ssl/certs/self-signed.pem -keyout /etc/ssl/private/self-signed.key -subj "/C=AU/ST=ACT/L=Canberra/O=Ben Formosa/OU=Fleet Manager/CN=fleetmanager.fm.internal"
 sudo chmod 600 /etc/ssl/certs/self-signed.pem
 sudo chmod 600 /etc/ssl/private/self-signed.key
 
-echo "### Create initscript"
-sudo useradd --shell /bin/false --system --user-group -M passenger
+# Create an init script
 echo '#!/bin/bash
 ### BEGIN INIT INFO
 # Provides:          fml
@@ -32,13 +35,13 @@ echo '#!/bin/bash
 ### END INIT INFO
 
 APP=fml
-APPPATH="/var/apps/$APP"
+APPPATH="/srv/rails/$APP"
 PIDFILE="/var/run/$APP.pid"
 LOGFILE="/var/log/$APP.log"
 PORT=443
 SSL_CERT=/etc/ssl/certs/self-signed.pem
 SSL_KEY=/etc/ssl/private/self-signed.key
-USER=passenger
+USER=fml
 
 SCRIPT="cd $APPPATH; passenger start -p $PORT --environment production --ssl --ssl-certificate $SSL_CERT --ssl-certificate-key $SSL_KEY --daemonize --user $USER --log-file $LOGFILE --pid-file $PIDFILE"
 
@@ -90,23 +93,37 @@ else
 fi' | sudo tee /etc/init.d/fml > /dev/null
 sudo chmod +x /etc/init.d/fml
 
-echo "### Install Rails"
-sudo apt-get install -y ruby ruby-dev zlib1g-dev libsqlite3-dev
-sudo gem install rails --no-ri --no-rdoc
-sudo gem install rake --no-ri --no-rdoc
+# Install required software with gem
+sudo gem install passenger --no-ri --no-rdoc
+sudo gem install bundler --no-ri --no-rdoc
 
-echo "### Install app"
-sudo apt-get install -y git
-git clone https://github.com/benformosa/fml.git
-sudo mkdir /var/apps
-sudo ln -s ~/fml /var/apps
-cd fml
-bundle install --deployment
-echo "Fml::Application.config.secret_key_base = '$(bundle exec rake secret)'" | tee --append config/initializers/secret_token.rb > /dev/null
+# Create the database setup script
+echo '#!/bin/bash
+sudo --login --user=fml --set-home sh -c "bundle exec rake db:setup RAILS_ENV=production"' | sudo tee /srv/rails/fml/setupdb.sh > /dev/null
+sudo chown fml:fml /srv/rails/fml/setupdb.sh
+sudo chmod +x /srv/rails/fml/setupdb.sh
+
+# Create a script to run app install as user fml
+echo '# Clone project from github.com
+sudo --login --user=fml --set-home sh -c "git init ."
+sudo --login --user=fml --set-home sh -c "git remote remove origin"
+sudo --login --user=fml --set-home sh -c "git remote add -t master -f origin https://github.com/benformosa/fml.git"
+sudo --login --user=fml --set-home sh -c "git checkout master"
+# Set preference to not install rdocs
+sudo --login --user=fml --set-home sh -c "echo \"gem: --no-ri --no-rdoc\" > ~/.gemrc"
+# Install required software with bundler
+sudo --login --user=fml --set-home sh -c "bundle install --deployment"
+# Prepare static assets
+sudo --login --user=fml --set-home sh -c "RAILS_ENV=production bundle exec rake assets:precompile"' | sudo tee /srv/rails/fml/fmlinstall.sh > /dev/null
+sudo chown fml:fml /srv/rails/fml/fmlinstall.sh
+sudo chmod +x /srv/rails/fml/fmlinstall.sh
+
+sudo /srv/rails/fml/fmlinstall.sh
+
+# Set secret token
+echo "Fml::Application.config.secret_key_base = \"$(cd /srv/rails/fml && bundle exec rake secret)\"" | sudo tee /srv/rails/fml/config/initializers/secret_token.rb > /dev/null
 
 echo
-echo $'Edit ~/fml/config/ldap.yml with your LDAP server details'
-echo $'Then run `bundle exec rake db:setup` to create database'
-echo $'run `/etc/init.d/fml start` to start app'
-
-popd
+echo $'Edit /srv/rails/fml/config/ldap.yml with your LDAP server details'
+echo $'Then run `sudo /srv/rails/fml/setupdb.sh` to create database'
+echo $'run `sudo /etc/init.d/fml start` to start app'
