808858a9fd734c8cdf228c81798f2a35413577f7
Ben Formosa <ben.formosa@gmail.com>
2015-08-18 20:40:31 +1000

    uninstaller

--- install/install.sh
+++ install/install.sh
@@ -36,11 +36,10 @@ sudo gem install passenger --no-ri --no-rdoc
 sudo gem install bundler --no-ri --no-rdoc
 
 # Download script to run app install as user fml
-sudo wget -q https://raw.githubusercontent.com/benformosa/fml/master/install/fminstall.sh -O /srv/rails/fml/fminstall.sh
-sudo chown fml:fml /srv/rails/fml/fminstall.sh
-sudo chmod +x /srv/rails/fml/fminstall.sh
+sudo wget -q https://raw.githubusercontent.com/benformosa/fml/master/install/fminstall.sh -O ~/fminstall.sh
+sudo chmod +x ~/fminstall.sh
 
-sudo /srv/rails/fml/fminstall.sh
+sudo ~/fminstall.sh
 
 # Set secret token
 echo "Fml::Application.config.secret_key_base = \"$(cd /srv/rails/fml && bundle exec rake secret)\"" | sudo tee /srv/rails/fml/config/initializers/secret_token.rb > /dev/null
---
+++ install/uninstall.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+BACKUPFILE=fml-production-backup_$(date +%s).sqlite3
+
+function uninstall {
+  # Stop application
+  sudo /etc/init.d/fml stop
+
+  echo "Backup database (Size $(du -h /srv/rails/fml/db/production.sqlite3 | cut -f1)) to $(echo ~/$BACKUPFILE) ?"
+  select yn in "Yes" "No" "Cancel uninstall"; do
+      case $yn in
+	  Yes ) backup; break;;
+	  No ) break;;
+	  Cancel uninstall ) exit;;
+      esac
+  done
+
+  # Remove self-signed certificate
+  sudo rm /etc/ssl/certs/self-signed.pem
+  sudo rm /etc/ssl/private/self-signed.key
+
+  # Remove init script
+  sudo rm /etc/init.d/fml
+  sudo rm /etc/fml.sh
+
+  # Remove application directory
+  sudo rm -rf /srv/rails/fml
+  
+  # Remove installer script
+  sudo rm ~/fminstall.sh
+
+  echo 'You may want to do the following to complete the uninstallation:
+`sudo apt-get remove ruby-dev zlib1g-dev libsqlite3-dev openssl git`
+`sudo gem uninstall bundler`
+`sudo gem uninstall passenger`
+`sudo userdel fml`
+`sudo groupdel fml`'
+}
+
+function backup {
+  # Make copy of database
+  sudo cp /srv/rails/fml/db/production.sqlite3 ~/$BACKUPFILE
+  sudo chown `whoami`:`whoami` ~/$BACKUPFILE
+  echo "Database backed up to $(echo ~/$BACKUPFILE)"
+}
+
+echo "
+
+###########
+#         #
+# WARNING #
+#         #
+###########
+
+This script will delete the following files and directories:
+/etc/fml.sh
+/etc/init.d/fml
+/etc/ssl/certs/self-signed.key
+/etc/ssl/certs/self-signed.pem
+/srv/rails/fml
+
+Are you sure you want to uninstall Fleet Manager?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) uninstall; break;;
+        No ) exit;;
+    esac
+done
