53cb1f68be8d50dd3757a19d03401444478d7671
Ben Formosa <ben.formosa@gmail.com>
2015-09-16 23:34:33 +1000

    assume test ldap is available

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 describe Trip do
   it "has a valid factory" do
-    user = FactoryGirl.create(:user)
+    user = User.where(login: 'user1').last
     car = FactoryGirl.create(:car)
     expect(trip1 = FactoryGirl.create(:trip, car: car, user: user)).to be_valid
   end
@@ -18,13 +18,15 @@ describe Trip do
 
   it "returns if it is the first trip for a car" do
     car = FactoryGirl.create(:car)
+    trip = Trip.where(car_id: car.id).first
+    puts trip
     expect(Trip.where(car_id: car.id).first.initial_trip?).to be true
   end
 
   it "has a previous trip unless it is an initial trip"
 
   it "has an odo greater than that of the previous trip" do
-    user = FactoryGirl.create(:user)
+    user = User.where(login: 'user1').last
     car = FactoryGirl.create(:car)
     trip1 = FactoryGirl.create(:trip, odo: 100, car: car, user: user)
     expect(FactoryGirl.build(:trip, odo: 99, car: car, user: user)).not_to be_valid
